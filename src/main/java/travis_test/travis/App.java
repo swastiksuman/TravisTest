77ca4ba65955518e63dd7906af3cb84d4cebaf14
swastiksuman@gmail.com <swastiksuman@gmail.com>
2015-07-28 23:59:38 +0530

    first commit

---
+++ src/main/java/travis_test/travis/App.java
@@ -0,0 +1,13 @@
+package travis_test.travis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/travis_test/travis/AppTest.java
@@ -0,0 +1,36 @@
+package travis_test.travis;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp(){
+		System.out.println("Some Setup");
+	}
+	
+	@Test
+	public void test(){
+		driver=new FirefoxDriver();
+		driver.get("http://www.google.com/");
+		System.out.println("Google Opened");
+	
+	}
+	
+	@AfterTest
+	public void afterClean(){
+		driver.close();
+		driver.quit();
+	}
+
+}
